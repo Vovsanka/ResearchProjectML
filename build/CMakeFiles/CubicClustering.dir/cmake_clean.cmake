77bf5cb2c7015d42705f7c78d6836a10ce9d7d06
Vovsanka <drobitkovolodya@gmail.com>
2025-04-21 16:56:46 +0200

    initial commit (cmake project)

---
+++ build/CMakeFiles/CubicClustering.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CubicClustering.dir/main.cpp.o"
+  "CMakeFiles/CubicClustering.dir/main.cpp.o.d"
+  "CubicClustering"
+  "CubicClustering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CubicClustering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World of Partially Optimal Cubic Subspace Clustering!" << std::endl;
+    return 0;
+}
