70f6a25f0958f40d1aeaab6acf2353c8bb19ad3d
Vovsanka <drobitkovolodya@gmail.com>
2025-05-14 14:16:02 +0200

    restructuring for recursive solving & min cut solving (including global min cut) for bidirectional graphs

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@
 #include <functional>
 #include <stdexcept>
 #include <algorithm>
-#include <boost/graph/push_relabel_max_flow.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/stoer_wagner_min_cut.hpp>
+#include <boost/property_map/property_map.hpp>
+
 
 std::pair<int, std::vector<bool>> solveMinCut(int vertices, std::vector<std::tuple<int,int,int>> edges, int s, int t) {
-    // digraph edges !!!
-    
+    // Bigraph!
     using namespace boost;
 
     // Define the graph type
@@ -33,14 +35,18 @@ std::pair<int, std::vector<bool>> solveMinCut(int vertices, std::vector<std::tup
         if (c < 0) throw std::runtime_error("MinCutProblem: negative edges are not allowed!");
         auto e1 = add_edge(u, v, g).first;
         auto e1r = add_edge(v, u, g).first;
-        capacity[e1] = c; 
-        capacity[e1r] = 0;
+        auto e2 = add_edge(v, u, g).first; 
+        auto e2r = add_edge(u, v, g).first;
+        capacity[e1] = capacity[e2] = c; 
+        capacity[e1r] = capacity[e2r] = 0;
         rev[e1] = e1r;
         rev[e1r] = e1;
+        rev[e2] = e2r;
+        rev[e2r] = e2;
     };
 
     // Compute max flow
-    int maxFlow = push_relabel_max_flow(g, s, t);
+    int minCut = push_relabel_max_flow(g, s, t);
 
     std::vector<bool> visited(vertices, false);
 
@@ -57,9 +63,32 @@ std::pair<int, std::vector<bool>> solveMinCut(int vertices, std::vector<std::tup
     // Run DFS on the residual network to determine the min cut subset
     dfs(s);
 
-    return std::make_pair(maxFlow, visited);
+    return std::make_pair(minCut, visited);
 }
 
+
+std::pair<int, std::vector<bool>> solveGlobalMinCut(std::vector<std::tuple<int,int,int>> edges) {
+    using namespace boost;
+
+    typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_weight_t, int>> Graph;
+    typedef property_map<Graph, edge_weight_t>::type WeightMap;
+
+    Graph g;
+    for (auto& e : edges) {
+        auto [u, v, c] = e;
+        if (c < 0) throw std::runtime_error("MinCutProblem: negative edges are not allowed!");
+        add_edge(u, v, c, g);
+    };
+
+    WeightMap weight_map = get(edge_weight, g);
+
+    std::vector<bool> parity(num_vertices(g));
+    auto parity_map = make_iterator_property_map(parity.begin(), get(vertex_index, g));
+
+    int minCut = stoer_wagner_min_cut(g, weight_map, boost::parity_map(parity_map));
+
+    return std::make_pair(minCut, parity);
+}
 
 
 
@@ -70,6 +99,7 @@ class UnorderedTriple { // unordered triple (sorted in the ascending order)
 public:
 
     UnorderedTriple(S s1, S s2, S s3) {
+        if (s1 == s2 || s1 == s2 || s2 == s3) throw std::runtime_error("Unordered triple cannot contain the same elements!");
         s = {s1, s2, s3};
         std::sort(std::begin(s), std::end(s));
     }
@@ -85,6 +115,10 @@ public:
         if (s[1] > other[1]) return false;
         return s[2] < other[2];
     }
+
+    bool contains(S s1) {
+        return (s[0] == s1 || s[1] == s1 || s[2] == s1);
+    }
 };
 
 
@@ -97,10 +131,10 @@ class CubicSetPartitionProblem {
     std::map<UnorderedTriple<>, int> tripleCosts;
     std::vector<std::vector<std::pair<int, int>>> relevantTriples;
     std::vector<std::vector<std::pair<int, int>>> negativeTriples;
-    std::vector<std::vector<std::pair<int, int>>> positiveTriples;
 
     std::vector<int> indexClusterMapping;
- 
+    int resultingCost;
+
     void initTripleCosts() {
         for (int i = 0; i < samples.size(); i++) {
             for (int j = i + 1; j < samples.size(); j++) {
@@ -117,11 +151,6 @@ class CubicSetPartitionProblem {
                         negativeTriples[i].push_back(std::make_pair(j, k));
                         negativeTriples[j].push_back(std::make_pair(i, k));
                         negativeTriples[k].push_back(std::make_pair(i, j));
-                    }
-                    if (c > 0) {
-                        positiveTriples[i].push_back(std::make_pair(j, k));
-                        positiveTriples[j].push_back(std::make_pair(i, k));
-                        positiveTriples[k].push_back(std::make_pair(i, j));
                     }
                 }
             }
@@ -167,19 +196,6 @@ class CubicSetPartitionProblem {
         return result;
     }
     
-    
-    
-public: 
-    explicit CubicSetPartitionProblem(const std::vector<S>& givenSamples, const std::function<int(UnorderedTriple<S>)> costCB)
-    : samples(givenSamples), cost(costCB) {
-        std::sort(std::begin(samples), std::end(samples)); // sort the samples in the ascending order
-        relevantTriples.resize(samples.size(), {});
-        negativeTriples.resize(samples.size(), {});
-        positiveTriples.resize(samples.size(), {});
-        indexClusterMapping.resize(samples.size(), 0);
-        initTripleCosts();
-    }
-
     int getCost(UnorderedTriple<S> t) {
         return cost(t);
     }
@@ -188,8 +204,83 @@ public:
         return cost(UnorderedTriple<S>(s1, s2, s3));
     }
 
+    void solve1() {
+        // // subset join criterion 3.11
+        // // sort negative triples
+        // std::vector<std::pair<int,UnorderedTriple<>> neg;
+        // for (int i = 0; i < samples.size(); i++) {
+        //     for (auto [j, k] : negativeTriples) {
+        //         UnorderedTriple<> indexTriple(i, j, k);
+        //         int c = tripleCosts[indexTriple];
+        //         neg.push_back(std::make_pair(c, indexTriple));
+        //     }
+        // }
+        // std::sort(std::begin(neg), std::end(neg));
+        // // heuristically construct and check the candidate sets R upon joining
+        // for (int i = 0; i < samples.size(); i++) {
+        //     for (int j = i + 1; j < samples.size(); j++) {
+        //         std::set<int> elementsR = {i, j}; // R must not be a connected component
+        //         for (auto [c, indexTriple] : neg) {
+        //             std::array<int,3>  = {indexTriple[0], indexTriple[1], indexTriple[2]};
+        //             for (int )
+
+        //         }   
+        //         // TODO: use Boost Stoer & Wagner algo to find the global minimal cut !!!
+
+        //         // std::set<int> candidates;
+        //         // for (int k = 0; k < samples.size(); k++) {
+        //         //     if (k != i && k != j) {
+        //         //         candidates.insert(k);
+        //         //     }
+        //         // }
+        //         // while (!candidates.empty()) {
+        //         //     std::vector<int> c(samples.size(), 0);
+        //         //     for (auto k : candidates) {
+        //         //         for (auto [k1, k2] : positiveTriples[k]) {
+        //         //             if (elementsR.count(k1) > 0 && elementsR.count(k2) > 0) {
+        //         //                 candidates.erase(k);
+        //         //             }
+        //         //         }
+        //         //     }
+        //         //     if (candidates.empty()) break;
+        //         //     int bestCandidate = *(std::begin(candidates));
+        //         //     for (auto k : candidates) {
+        //         //         for (auto [k1, k2] : negativeTriples[k]) {
+        //         //             if (elementsR.count(k1) > 0 && elementsR.count(k2) > 0) {
+        //         //                 auto indexTriple = UnorderedTriple<>(k, k1, k2);
+        //         //                 c[k] += tripleCosts[indexTriple];
+        //         //             }
+        //         //         }
+        //         //         if (c[k] < c[bestCandidate]) {
+        //         //             bestCandidate = k;
+        //         //         }
+        //         //     }
+        //         //     elementsR.insert(bestCandidate);
+        //         //     candidates.erase(bestCandidate);
+        //         // }
+        //         // // check if elementsR can be merged
+        //         // auto [solveMinCut]
+        //     }
+        // }
+    }
+    
+    void solve2() {
+        // TODO
+        // just for now: identity clustering
+    }
+    
+public: 
+    explicit CubicSetPartitionProblem(const std::vector<S>& givenSamples, const std::function<int(UnorderedTriple<S>)> costCB)
+    : samples(givenSamples), cost(costCB) {
+        std::sort(std::begin(samples), std::end(samples)); // sort the samples in the ascending order
+        relevantTriples.resize(samples.size(), {});
+        negativeTriples.resize(samples.size(), {});
+        indexClusterMapping.resize(samples.size(), 0);
+        resultingCost = 0;
+        initTripleCosts();
+    }
+
     std::map<S, int> getClusterMapping() {
-        if (indexClusterMapping.empty()) throw std::runtime_error("The problem has not been solved yet!");
         std::map<S, int> result;
         for (int i = 0; i < samples.size(); i++) {
             result[samples[i]] = indexClusterMapping[i];
@@ -197,16 +288,22 @@ public:
         return result;
     } 
 
+    int getResultingCost() {
+        return resultingCost;
+    }
+
     void solve() {
+        std::vector<CubicSetPartitionProblem<S>> subproblems = getSubproblemsWithRegionGrowing();
         // init index of 
         std::map<S, int> indexOf;
         for (int i = 0; i < samples.size(); i++) {
             indexOf[samples[i]] = i;
         }
-        std::vector<CubicSetPartitionProblem<S>> subproblems = getSubproblemsWithRegionGrowing();
+        // unite the solutions of the independent subproblems
         int clusterOffset = 0;
-        for (auto& subproblem : subproblems) { // unite the solutions of the independent subproblems
-            subproblem.solveAsSubproblem();
+        for (auto& subproblem : subproblems) { 
+            subproblem.solve1();
+            resultingCost += subproblem.getResultingCost();
             int subclusterCount = 1;
             for (auto [sample, subcluster] : subproblem.getClusterMapping()) {
                 indexClusterMapping[indexOf[sample]] = subcluster + clusterOffset;
@@ -215,13 +312,8 @@ public:
             clusterOffset += subclusterCount;
         }
     }
-
-    void solveAsSubproblem() {
-        // TODO !!!
-        // just for now: all samples beint to the same cluster
-    }
-
-    std::pair<int, std::vector<bool>> solveMinCutForIndexSubset(std::vector<bool> indexSubset, bool invertCosts, int s, int t) {
+    
+    std::pair<int, std::vector<bool>> solveMinCutForIndexSubset(bool globalMinCut, std::vector<bool> indexSubset, bool invertCosts, int s = 0, int t = 0) {
         int vertices = std::count(std::begin(indexSubset), std::end(indexSubset), true);
         std::vector<int> indexMapping(indexSubset.size());
         std::vector<int> invIndexMapping(vertices);
@@ -257,13 +349,19 @@ public:
                 int c = adjMatrix[i_node][j_node] / 3; // since each triple has been considered 3 times
                 if (c) {
                     edges.push_back(std::make_tuple(i_node, j_node, c));
-                    edges.push_back(std::make_tuple(j_node, i_node, c));
                 }
             }
         } 
 
         // solve the MinCut problem
-        auto [minCut, partition] = solveMinCut(vertices, edges, indexMapping[s], indexMapping[t]);
+        std::pair<int, std::vector<bool>> solution;
+        if (globalMinCut) {
+            solution = solveGlobalMinCut(edges);
+        } else {
+            solution = solveMinCut(vertices, edges, indexMapping[s], indexMapping[t]);
+        }
+        int minCut = solution.first;
+        std::vector<bool> partition = solution.second;
 
         // transform the results to the original domain
         std::vector<bool> partitionOnSamples(indexSubset.size(), false);
@@ -274,6 +372,8 @@ public:
         // divide the MinCut result by 2 because all triples have been transformed with a double cost
         return std::make_pair(minCut/2, partitionOnSamples);
     }
+
+
 };
 
 
@@ -313,7 +413,7 @@ int main() {
     
     std::vector<bool> indexSubset(5, true);
     // indexSubset[1] = false;
-    auto [minCut, partition] = problem.solveMinCutForIndexSubset(indexSubset, true, 0, 3);
+    auto [minCut, partition] = problem.solveMinCutForIndexSubset(true, indexSubset, true);
     std::cout << minCut << std::endl;
     for (int i = 0; i < 4; i++) {
         if (partition[i]) {
